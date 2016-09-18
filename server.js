0c5234edc3870105fcedd7303db9844711813466
NavneetKaur9 <pannu.navneet9@gmail.com>
2016-09-19 03:54:43 +0530

    server port

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = express(),
 	publicDir = require('path').join(__dirname);
 app.use(express.static(publicDir));
 // app.use('/', express.static('index.html'));
-app.listen(8000);
+app.listen(process.env.PORT || 8000);
 console.log('server running on port:8000');
